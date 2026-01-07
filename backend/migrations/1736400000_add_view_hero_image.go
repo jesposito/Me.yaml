21b9b206ae79a343318f92cea757d1d6b376a514
Jed Esposito <egerthe@gmail.com>
2026-01-07 22:32:40 +1300

    Fix migration to use pocketbase's m.Register() pattern

--- backend/migrations/1736400000_add_view_hero_image.go
+++ backend/migrations/1736400000_add_view_hero_image.go
@@ -2,42 +2,26 @@ package migrations
 
 import (
 	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
 )
 
 func init() {
-	Migrations = append(Migrations, Migration{
-		Name: "1736400000_add_view_hero_image",
-		Up: func(app core.App) error {
-			collection, err := app.FindCollectionByNameOrId("views")
-			if err != nil {
-				return err
-			}
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("views")
+		if err != nil {
+			return err
+		}
 
-			if collection.Fields.GetByName("hero_image") != nil {
-				return nil
-			}
-
-			collection.Fields.Add(&core.FileField{
-				Name:      "hero_image",
-				MaxSize:   10485760,
-				MimeTypes: []string{"image/jpeg", "image/png", "image/webp"},
-			})
-
-			return app.Save(collection)
-		},
-		Down: func(app core.App) error {
-			collection, err := app.FindCollectionByNameOrId("views")
-			if err != nil {
-				return err
-			}
-
-			field := collection.Fields.GetByName("hero_image")
-			if field != nil {
-				collection.Fields.RemoveById(field.GetId())
-				return app.Save(collection)
-			}
-
+		if collection.Fields.GetByName("hero_image") != nil {
 			return nil
-		},
-	})
+		}
+
+		collection.Fields.Add(&core.FileField{
+			Name:      "hero_image",
+			MaxSize:   10485760,
+			MimeTypes: []string{"image/jpeg", "image/png", "image/webp"},
+		})
+
+		return app.Save(collection)
+	}, nil)
 }
